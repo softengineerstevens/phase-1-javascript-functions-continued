326e2ea4b307d2de8a9debb51b387555ea4b9288
softengineerstevens <softengineerstevens@gmail.com>
2024-06-30 07:59:53 +0300

    functions more

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+console.log(saturdayFun());
+console.log(saturdayFun("I want to bathe my dog!"));
+
+// mondayWork
+
+function mondayWork (activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+console.log(mondayWork());
+console.log(mondayWork('work from home'));
+
+
+function wrapAdjective(symbol){
+    return function (adjective){
+        return `You are ${symbol}${adjective}${symbol}!`
+
+    }
+
+}
+
+console.log(wrapAdjective('/')('so amazing'));
